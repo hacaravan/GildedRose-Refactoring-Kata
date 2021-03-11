f5c6428b2f7f058897220b09d49139b17961f203
hacaravan <hacavanagh@gmail.com>
2021-03-11 12:57:02 +0000

    Adding context to first test

--- js-jest/test/gilded_rose.test.js
+++ js-jest/test/gilded_rose.test.js
@@ -1,9 +1,14 @@
 const {Shop, Item} = require("../src/gilded_rose");
 
 describe("Gilded Rose", function() {
-  it("should foo", function() {
-    const gildedRose = new Shop([new Item("foo", 0, 0)]);
-    const items = gildedRose.updateQuality();
-    expect(items[0].name).toBe("foo");
-  });
+  describe("when you add items to a new shop", () => {
+    describe("updateQuality()", () => {
+      it("returns objects you've added to the shop", function() {
+        const gildedRose = new Shop([new Item("foo", 0, 0)]);
+        const items = gildedRose.updateQuality();
+        expect(items[0].name).toBe("foo");
+      });
+    })
+  })
+
 });
